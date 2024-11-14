94014f8542cd8877b6444a4f8128eb601c7f392d
ninxsk <ninoskaparedes@gmail.com>
2024-11-14 19:34:35 +0000

    Chore: se agrega nuevo serealizador para asistente

--- backend/backend/Asistencia/serializers.py
+++ backend/backend/Asistencia/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import ListaAsistencia
+from .models import ListaAsistencia , Asistente
 
 
 class ListaAsistenciaSerializer(serializers.ModelSerializer):
     
-    class meta:
+    class Meta:
         model = ListaAsistencia
-        fields = ['id','carrera','asistente','taller','correo','comentario', 'satisfaccion']
+        fields = ['id_asiste','carrera','asistente','taller','correo','comentario', 'satisfaccion']
+        
+
+class AsisteSerializaer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Asistente
+        fields =['id','rut','nombre']
+        
+
+

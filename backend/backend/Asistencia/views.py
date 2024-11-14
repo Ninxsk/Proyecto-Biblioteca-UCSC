751b76c5c700bf43ff822af618f768487f9d8e1f
ninxsk <ninoskaparedes@gmail.com>
2024-11-14 19:37:24 +0000

    Chore : se crea vista para los asistentes

--- backend/backend/Asistencia/views.py
+++ backend/backend/Asistencia/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import ListaAsistencia
-from .serializers import ListaAsistenciaSerializer
+from .models import ListaAsistencia, Asistente
+from .serializers import ListaAsistenciaSerializer , AsisteSerializaer
 
 class ListaAsistenciaViewset(viewsets.ModelViewSet):
     serializer_class=ListaAsistenciaSerializer
     queryset =ListaAsistencia.objects.all()
+    
+
+class AsistenteViewset(viewsets.ModelViewSet):
+    serializer_class= AsisteSerializaer
+    queryset=Asistente.objects.all()
     
 
 # Create your views here.

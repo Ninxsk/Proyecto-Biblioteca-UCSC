29a78bff3c221279a3561946987ac9e46546cf76
ninxsk <ninoskaparedes@gmail.com>
2024-11-14 19:35:19 +0000

    Chore:Se agregan ruta para asistente

--- backend/backend/Asistencia/urls.py
+++ backend/backend/Asistencia/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import ListaAsistenciaViewset
-
+from .views import ListaAsistenciaViewset , AsistenteViewset
 router_lista=DefaultRouter()
-router_lista.register(prefix='ListaAsistencia',viewset=ListaAsistenciaViewset)
+router_lista.register(prefix='ListaAsistencia',viewset=ListaAsistenciaViewset),
+router_lista.register(prefix='Asistente',viewset=AsistenteViewset)

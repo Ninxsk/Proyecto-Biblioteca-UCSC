07dc8537faaa58d79cd5028f51977ff6165ea8bc
Ninxsk <ninoskaparedes@gmail.com>
2024-11-14 08:52:13 -0300

    chore: actualizacion views Se actualiza clase TallerViewSet con los respectivos serializadores creados , ademas de agregar filtros de busqueda

--- backend/backend/Taller/views.py
+++ backend/backend/Taller/views.py
@@ -1,8 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Taller
-from .serializers import TallerSerializer
+from .serializers import TallerCreateSerializer, TallerListSerializer, TallerDetailSerializer, TallerUpdateSerializer
 
 class TallerViewSet(viewsets.ModelViewSet):
     queryset = Taller.objects.all()
-    serializer_class = TallerSerializer
-   
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter] 
+    filterset_fields = ['nombre', 'fecha', 'modalidad', 'relator']  
+    search_fields = ['nombre', 'relator']  
+    
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TallerListSerializer
+        elif self.action == 'retrieve':
+            return TallerDetailSerializer
+        elif self.action in ['update', 'partial_update']:
+            return TallerUpdateSerializer
+        return TallerCreateSerializer

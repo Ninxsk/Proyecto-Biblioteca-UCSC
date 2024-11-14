c8c93b14ca85aea8e07cdab14dc262fc3fc3e53b
Ninxsk <ninoskaparedes@gmail.com>
2024-11-14 08:54:54 -0300

    Chore: actualizacion de settings Se agrega django_filters a la app y se crea un res_framework para poder trabajar con los filtros

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -43,10 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'Taller'
 
     ,
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+         'rest_framework.filters.SearchFilter'
+    ],
+  
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -138,3 +147,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

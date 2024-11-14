74913e7b1c40bb56b8585f9aa2931deba38af924
Ninxsk <ninoskaparedes@gmail.com>
2024-11-14 08:59:27 -0300

    Chore: cometarios
    Se agregan comentarios a los serializadores

--- backend/backend/Taller/serializers.py
+++ backend/backend/Taller/serializers.py
@@ -1,21 +1,24 @@
 from rest_framework import serializers
 from .models import Taller
 
+#Creacion taller
 class TallerCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Taller
         fields = ['nombre', 'relator', 'fecha', 'inicio', 'fin', 'modalidad', 'solicitud', 'jornada', 'lugar']
 
     def validate(self, data):
-        # Verifica que la hora de inicio sea anterior a la hora de fin
+        
         if data['inicio'] >= data['fin']:
             raise serializers.ValidationError("La hora de inicio debe ser anterior a la hora de término.")
         
-        # Verifica que no exista otro taller asociado a la misma solicitud
+       
         if Taller.objects.filter(solicitud=data['solicitud']).exists():
             raise serializers.ValidationError("Ya existe un taller asociado a esta solicitud.")
 
         return data
+    
+#Listar todos los talleres    
 class TallerListSerializer(serializers.ModelSerializer):
     solicitud = serializers.SerializerMethodField()
 
@@ -24,13 +27,13 @@ class TallerListSerializer(serializers.ModelSerializer):
         fields = ['id','nombre', 'fecha', 'solicitud']
 
     def get_solicitud(self, obj):
-        # Devuelve el ID de la solicitud si existe, de lo contrario None
+    
         return obj.solicitud.id if obj.solicitud else None
 
 
 
 
-
+#Dtalle de cada taller
 class TallerDetailSerializer(serializers.ModelSerializer):
     solicitud = serializers.PrimaryKeyRelatedField(read_only=True)
     carrera = serializers.SerializerMethodField()
@@ -70,11 +73,7 @@ class TallerDetailSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        
-        # Convertir modalidad a "Presencial" o "Online"
         representation['modalidad'] = "Presencial" if instance.modalidad else "Online"
-        
-        # Elimina los campos `solicitud`, `jornada`, `carrera` y `facultad` si no están presentes
         if representation.get('solicitud') is None:
             representation.pop('solicitud')
         if representation.get('jornada') is None:
@@ -87,6 +86,8 @@ class TallerDetailSerializer(serializers.ModelSerializer):
         return representation
 
 
+
+#Actualizacion taller
 class TallerUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model=Taller

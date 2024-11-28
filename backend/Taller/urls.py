1d7387f8082c3b7e59f4a9e8efaf75b613e5cdf1
ninxsk <ninoskaparedes@gmail.com>
2024-11-28 20:36:55 +0000

    Chore: se agrega ruta asistencia

--- backend/Taller/urls.py
+++ backend/Taller/urls.py
@@ -1,8 +1,11 @@
-
 from rest_framework.routers import DefaultRouter
-from .views import TallerViewSet
+from .views import TallerViewSet,AsistenciaViewSet,JornadaViewSet
+from django.urls import path, include
 
 router_taller=DefaultRouter()
 router_taller.register(prefix='talleres', viewset=TallerViewSet)
+router_taller.register(prefix='asistencia',viewset=AsistenciaViewSet)
 
-
+urlpatterns = [
+    path('', include(router_taller.urls)),
+]

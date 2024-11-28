7a5b1b359409e083ddbec0c50fec215f518dd0dd
ninxsk <ninoskaparedes@gmail.com>
2024-11-28 21:01:18 +0000

    Feat: Creacion de url para jornada

--- backend/Taller/urls.py
+++ backend/Taller/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 router_taller=DefaultRouter()
 router_taller.register(prefix='talleres', viewset=TallerViewSet)
 router_taller.register(prefix='asistencia',viewset=AsistenciaViewSet)
+router_taller.register(prefix='jornada',viewset=JornadaViewSet)
 
 urlpatterns = [
     path('', include(router_taller.urls)),

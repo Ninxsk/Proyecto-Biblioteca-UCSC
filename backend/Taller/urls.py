255a1571996a1266e75fb0184daf92dd4f72acd6
ninxsk <ninoskaparedes@gmail.com>
2024-12-04 02:57:11 +0000

    Chore: se agrega ruta para listar talleres con detalle

--- backend/Taller/urls.py
+++ backend/Taller/urls.py
@@ -1,8 +1,10 @@
 
 from rest_framework.routers import DefaultRouter
-from .views import TallerViewSet
+from .views import TallerViewSet , TallerCompletoViewSet
 
 router_taller=DefaultRouter()
 router_taller.register(prefix='talleres', viewset=TallerViewSet)
+router_taller.register(prefix='talleres-lista', viewset=TallerCompletoViewSet,basename='taller-lista')
+
 
 

5dfecc889081ffd3dcbfde06b0603aa43f79821e
ninxsk <ninoskaparedes@gmail.com>
2024-12-10 11:06:37 +0000

    ultimos cambios

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.getenv('DEBUG')
 
 
 ALLOWED_HOSTS = ['10.1.0.74']
-ALLOWED_HOSTS= ['127.0.0.1']
+#ALLOWED_HOSTS= ['127.0.0.1']
 #cnfigurar para ngnix
  
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_filters',
     'Taller',
     'Asistencia'
@@ -70,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -173,3 +175,5 @@ LOGGING = {
         },
     },
 }
+
+CORS_ALLOW_ALL_ORIGINS = True

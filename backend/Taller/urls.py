bb85bfb073a6ce8a1a1b88f6baa75339fc71aa6c
ninxsk <ninoskaparedes@gmail.com>
2024-12-11 17:46:50 +0000

    Feat: creacion ruta de solicitud

--- backend/Taller/urls.py
+++ backend/Taller/urls.py
@@ -1,8 +1,10 @@
 
 from rest_framework.routers import DefaultRouter
-from .views import TallerViewSet
+from .views import TallerViewSet,SolicitudViewSet
 
 router_taller=DefaultRouter()
+
 router_taller.register(prefix='talleres', viewset=TallerViewSet)
+router_taller.register(prefix='solicitudes',viewset=SolicitudViewSet)
 
 
